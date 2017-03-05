a600e5f18c3ef337a181bc9e3dbc0e380fd885a6
LordHub <anpaumuo@inf.upv.es>
2017-03-05 12:17:56 +0100

    Now crops the images and removes the Watermark
    If the watermark is in the top right side it can be seen after the crop. It also happens with the horizontal pictures.

--- Crop.py
+++ Crop.py
@@ -1,13 +1,15 @@
-from PILLOW import Image
-img = Image.open("GICOMBAT.jpg")
+from PIL import Image
+img = Image.open("SUPERMAN_TC_RS.jpg")
 half_the_width = img.size[0] / 2
 half_the_height = img.size[1] / 2
+reduce_the_width = half_the_width - half_the_width / 15
+reduce_the_height = half_the_height - half_the_height / 15
 img4 = img.crop(
     (
-        half_the_width - 50,
-        half_the_height - 75,
-        half_the_width + 50,
-        half_the_height + 75
+        half_the_width - reduce_the_width,
+        half_the_height - reduce_the_height,
+        half_the_width + reduce_the_width,
+        half_the_height + reduce_the_height
     )
 )
-img4.save("img4.jpg")
+img4.save("img8h1.jpg")
