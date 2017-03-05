42249543d4bfe3fa5637398def1f4cbffa55f400
LordHub <anpaumuo@inf.upv.es>
2017-03-05 12:26:03 +0100

    Now its a function

--- Crop.py
+++ Crop.py
@@ -1,15 +1,17 @@
 from PIL import Image
-img = Image.open("SUPERMAN_TC_RS.jpg")
-half_the_width = img.size[0] / 2
-half_the_height = img.size[1] / 2
-reduce_the_width = half_the_width - half_the_width / 15
-reduce_the_height = half_the_height - half_the_height / 15
-img4 = img.crop(
-    (
-        half_the_width - reduce_the_width,
-        half_the_height - reduce_the_height,
-        half_the_width + reduce_the_width,
-        half_the_height + reduce_the_height
+def cropImage(filename):
+    img = Image.open("filename")
+    half_the_width = img.size[0] / 2
+    half_the_height = img.size[1] / 2
+    reduce_the_width = half_the_width - half_the_width / 15
+    reduce_the_height = half_the_height - half_the_height / 15
+    img4 = img.crop(
+        (
+            half_the_width - reduce_the_width,
+            half_the_height - reduce_the_height,
+            half_the_width + reduce_the_width,
+            half_the_height + reduce_the_height
+        )
     )
-)
-img4.save("img8h1.jpg")
+    img4.save("filename")
+    return
